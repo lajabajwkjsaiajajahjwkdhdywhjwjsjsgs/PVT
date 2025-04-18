18e7b155df7b7f7846f57be095d76a361fddb226
Angel Priya <abhishekpandit4224@gmail.com>
2025-04-18 13:00:21 +0530

    Update play.py

--- Shigaraki/utils/decorators/play.py
+++ Shigaraki/utils/decorators/play.py
@@ -200,20 +200,20 @@ def PlayWrapper(command):
 
 
 
-@app.on_message(filters.command("link") & filters.user(OWNER))
-async def link_handler(client, message):
-    if len(message.command) < 2:
-        return await message.reply_text("ᴘʀᴏᴠɪᴅᴇ ᴀ ᴄʜᴀᴛ ɪᴅ ᴛᴏ ʀᴇᴛʀɪᴇᴠᴇ ɪᴛs ʟɪɴᴋ.")
+#@app.on_message(filters.command("link") & filters.user(OWNER))
+#async def link_handler(client, message):
+#    if len(message.command) < 2:
+    #    return await message.reply_text("ᴘʀᴏᴠɪᴅᴇ ᴀ ᴄʜᴀᴛ ɪᴅ ᴛᴏ ʀᴇᴛʀɪᴇᴠᴇ ɪᴛs ʟɪɴᴋ.")
 
-    target_chat_id = message.command[1]
+#    target_chat_id = message.command[1]
 
-    try:
-        chat = await app.get_chat(int(target_chat_id))
-        if chat.username:
-            chat_link = f"https://t.me/{chat.username}"
-        else:
-            chat_link = await app.export_chat_invite_link(int(target_chat_id))
+   # try:
+  #      chat = await app.get_chat(int(target_chat_id))
+        #if chat.username:
+         #   chat_link = f"https://t.me/{chat.username}"
+      #  else:
+         #   chat_link = await app.export_chat_invite_link(int(target_chat_id))
 
-        await message.reply_text(f"ʜᴇʀᴇ ɪs ᴛʜᴇ ᴄʜᴀᴛ ʟɪɴᴋ ғᴏʀ {chat.title}: {chat_link}")
+      #  await message.reply_text(f"ʜᴇʀᴇ ɪs ᴛʜᴇ ᴄʜᴀᴛ ʟɪɴᴋ ғᴏʀ {chat.title}: {chat_link}")
 
     
